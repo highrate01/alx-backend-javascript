bd92c139dea9347fedaeca08d4aa51c7d63b548c
Olufemi Adebis <olufemiadebisi35@gmail.com>
2024-08-13 08:19:47 +0000

    tasks

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
+  return Array.from(set)
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
