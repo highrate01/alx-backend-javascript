25941b7244c42d0f7d3809fc1fed697a0444145d
Olufemi Adebis <olufemiadebisi35@gmail.com>
2024-08-14 18:49:04 +0000

    teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,17 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [property: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+console.log(teacher3);
