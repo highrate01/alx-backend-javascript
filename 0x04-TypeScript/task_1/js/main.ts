413f6afb6179187d23a6f5855a43706a9a85c153
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-08-14 23:50:59 +0100

    updated

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,3 +24,26 @@ function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 }
 console.log(printTeacher("John", "Doe"));
+
+interface Class {
+  workOnHomework(): string;
+  displayName(): string;
+}
+interface Constructor {
+  firstName: string;
+  lastName: string;
+}
+class StudentClass implements Class {
+  firstName: string;
+  lastName: string;
+  constructor(params: Constructor) {
+    this.firstName = params.firstName;
+    this.lastName = params.lastName;
+  }
+  workOnHomework() {
+    return "Currently working";
+  }
+  displayName() {
+    return this.firstName;
+  }
+}
