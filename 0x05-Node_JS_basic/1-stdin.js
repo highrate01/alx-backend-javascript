8f028687642cec6948fa39ae58dc44c27cd6450f
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-10-01 18:59:10 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,18 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name.toString().trim()}\n`);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
+
+  if (process.stdin.isTTY) {
+    // If input is from terminal (interactive), exit immediately
+    process.exit();
   }
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+// Listen for the 'exit' event, which will be triggered for piped input
+process.on('exit', () => {
+  // If the input is not interactive (piped), show the closing message
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+  }
 });
